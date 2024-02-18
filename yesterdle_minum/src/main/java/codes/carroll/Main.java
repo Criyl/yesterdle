95640acef8658912ccd90f42d8f64152fab6de1e
Chris Carroll <chris@carroll.codes>
2024-02-18 01:48:17 -0500

    rename directory name

--- yesterdle_minum/src/main/java/codes/carroll/Main.java
+++ yesterdle_minum/src/main/java/codes/carroll/Main.java
@@ -5,10 +5,8 @@ import com.renomad.minum.web.FullSystem;
 public class Main {
 
     public static void main(String[] args) {
-        // Start the system
         FullSystem fs = FullSystem.initialize();
 
-        // Register some endpoints
         new Registry(fs.getContext()).registerDomains();
 
         fs.block();
